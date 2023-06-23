76d78257abcbf1a9a7bcea0f21b5f864fd027c26
david <david@debian-BULLSEYE-live-builder-AMD64>
2023-06-23 14:45:06 -0500

    r

--- pregunta.py
+++ pregunta.py
@@ -10,10 +10,29 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
-
+import re
 
 def ingest_data():
-    df = pd.read_csv('clusters_report.txt', sep='|')
-    df.columns = df.columns.str.lower().str.replace(' ', '_')
-    df['palabras clave'] = df['palabras clave'].str.replace(' ', ', ')
-    return df
+    with open('clusters_report.txt') as archivo:
+        filas = archivo.readlines()
+
+    data = []
+    row = []
+    header = ['cluster', 'cantidad_de_palabras_clave', 
+    'porcentaje_de_palabras_clave', 'principales_palabras_clave']
+    for fila in filas[4:]:
+        if re.match('^ +\d+ +', fila):
+            a = fila.split()
+            row.append(int(a[0]))
+            row.append(int(a[1]))
+            row.append(float(a[2].replace(',','.')))
+            row.append(' '.join(a[4:]))
+        elif re.match('^ +\w', fila):
+            a = fila.split()
+            a = ' '.join(a)
+            row[3] += ' ' + a
+        elif re.match('^\n', fila) or re.match('^ +$', fila):
+            row[3] = row[3].replace('.', '')
+            data.append(row)
+            row = []
+    return pd.DataFrame (data, columns =header)
